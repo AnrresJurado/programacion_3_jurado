3283498eb6ded67808fe089d62ab9bf8bf7733c2
AnrresJurado <andrsflp68@hotmail.com>
2026-06-17 09:01:20 -0500

    Clase: 17-07-2026

--- modulo4/fruteria/src/main.tsx
+++ modulo4/fruteria/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppAnimales.tsx'
-//import ProductCard from './components/ProductCard'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
