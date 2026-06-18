5bf4bd54a03cc87c140bf0f59ad6082133ac68c1
AnrresJurado <andrsflp68@hotmail.com>
2026-06-18 09:16:26 -0500

    Clase: 18-07-2026

--- modulo4/fruteria/src/App.tsx
+++ modulo4/fruteria/src/App.tsx
@@ -3,14 +3,15 @@
 import WelcomeBanner       from './components/WelcomeBanner'
 import UserGreeting        from './components/UserGreeting'
 import CurrentDateDisplay  from './components/CurrentDateDisplay'
-/*import ColoredBox          from './components/ColoredBox'
+import ColoredBox          from './components/ColoredBox'
 import ConditionalGreeting from './components/ConditionalGreeting'
 import FruitList           from './components/FruitList'
 import PriceTag            from './components/PriceTag'
 import StatusBadge         from './components/StatusBadge'
 import MiniProfileCard     from './components/MiniProfileCard'
 import SimpleInfoTable     from './components/SimpleInfoTable'
-import ProductCard         from './components/ProductCard'
+import VehiculosTable  from './components/VehiculosTable'
+/*import ProductCard         from './components/ProductCard'
 import ProductCatalogList  from './components/ProductCatalogList'
 import UserProfileCard     from './components/UserProfileCard'*/
 
@@ -31,11 +32,14 @@ import UserProfileCard     from './components/UserProfileCard'*/
 // │  13  UserProfileCard     — ejercicio: props complejas + rol             │
 // └──────────────────────────────────────────────────────────────────────────┘
 
-const PASO = 3
+const PASO = 11
 const fruits = [
   { name: 'Manzana', emoji: '🍎', calories: 52 },
   { name: 'Banana',  emoji: '🍌', calories: 89 },
   { name: 'Naranja', emoji: '🍊', calories: 47 },
+  { name: 'Kiwi', emoji: '🥝', calories: 61 },
+  { name: 'Coco', emoji: '🥥', calories: 61 },
+  { name: 'Uvas', emoji: '🍇', calories: 61 }
 ]
 
 const catalog = [
@@ -50,20 +54,19 @@ export default function App() {
     PASO ===  1 ? <WelcomeBanner subtitle= "Programadores Estrellas"/> : 
     PASO ===  2 ? <><UserGreeting name="Ana García" occupation="Desarrolladora Frontend" /></>:
     PASO ===  3 ? <CurrentDateDisplay /> :
-    /*
     PASO ===  4 ? (
-      <div style={{ display: 'flex', gap: 12 }}>
-        <ColoredBox color="#0070f3" label="Primary" />
-        <ColoredBox color="#22c55e" label="Success" />
-        <ColoredBox color="#e00"    label="Danger" />
+      <div style={{ display: 'flex', gap: 12 , opacity: 0.5, borderRadius: 0}}>
+        <ColoredBox color="#f59e0b" label="Primary" width={120} height={40}  />
+        <ColoredBox color="#8b5cf6" label="Success" />
+        <ColoredBox color="#ec4899"  label="Danger" borderRadius={50}  />
       </div>
     ) :
-    PASO ===  5 ? <ConditionalGreeting isLoggedIn={true} userName="Ana" timeOfDay="afternoon" /> :
+    PASO ===  5 ? <ConditionalGreeting isLoggedIn={true} userName="Carlos" timeOfDay="night" /> :
     PASO ===  6 ? <FruitList fruits={fruits} title="Frutas favoritas" /> :
     PASO ===  7 ? (
       <div style={{ display: 'flex', gap: 24, alignItems: 'flex-end' }}>
-        <PriceTag amount={99.99} currency="USD" />
-        <PriceTag amount={99.99} currency="USD" discountPercent={20} />
+        <PriceTag amount={99.99} currency="EUR" />
+        <PriceTag amount={99.99} currency="USD" discountPercent={50} />
       </div>
     ) :
     PASO ===  8 ? (
@@ -72,6 +75,7 @@ export default function App() {
         <StatusBadge status="pending" />
         <StatusBadge status="error" />
         <StatusBadge status="inactive" />
+        <StatusBadge status="pending" label="En revisión" />
       </div>
     ) :
     PASO ===  9 ? (
@@ -82,7 +86,7 @@ export default function App() {
         status="active"
         joinedYear={2019}
       />
-    ) :
+    ) : 
     PASO === 10 ? (
       <SimpleInfoTable
         title="Resumen del pedido"
@@ -92,7 +96,22 @@ export default function App() {
           { label: 'Total',     value: '$94.99', highlight: true },
         ]}
       />
+    ) : 
+    PASO === 11 ? (
+      <VehiculosTable
+        title="Resumen del pedido"
+        rows={[
+          { label: 'Marca',  value: 'Subaru' },
+          { label: 'Modelo',   value: 'WRX' },
+          { label: 'Año',     value: '2026'},
+          { label: 'Precio',   value: '$60000.00'},
+          { label: 'Envio',   value: '$25.00'},
+          { label: 'Total',   value: '$60025.00', highlight: true },
+        ]}
+        
+      />
     ) :
+    /*
     PASO === 11 ? <ProductCard title="Teclado inalámbrico" description="Bluetooth 5.0, retroiluminado" highlighted /> :
     PASO === 12 ? <ProductCatalogList products={catalog} title="Productos disponibles" /> :
     PASO === 13 ? (
