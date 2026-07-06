fa6f2d3a29c8ff9cb37593c3fea6906e61c37784
AnrresJurado <andrsflp68@hotmail.com>
2026-07-06 08:59:12 -0500

    Clase: 06-07-2026

---
+++ modulo4/06-react-router/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+// src/pages/NotFoundPage.tsx
+
+export default function NotFoundPage() {
+    return (
+        <div>
+            <h1 style={{ marginTop: 12 }}>Home Page</h1>
+            <p style={{ color: '#6b7280' }}>
+                Bienvenido a una App con Router
+            </p>
+        </div>
+    )
+}
